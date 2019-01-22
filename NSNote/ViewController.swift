83c557ca01d72a066e933f2d67c12ebffbc85e60
Tunc Tugcu <tunctugcu@gmail.com>
2019-01-22 19:41:31 +0300

    Initial Commit

---
+++ NSNote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NSNote
+//
+//  Created by Tunc Tugcu on 22.01.2019.
+//  Copyright © 2019 Tunc Tugcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
